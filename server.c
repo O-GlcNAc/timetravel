725d55f467eed2f167bb21a2e79928589f0c69ce
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-12-03 12:29:07 +0900

    Update server.c

--- server.c
+++ server.c
@@ -2,34 +2,51 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 
 #define FIFOFILE "fifo"
+#define PYTHON_SCRIPT "mydb.py" // 파이썬 스크립트 파일명
 
 int main(int argc, char **argv) {
     int fd;
-    srand(time(NULL)); // 난수 생성을 위한 시드 설정
+    char buf[BUFSIZ];
 
-    if ((fd = open(FIFOFILE, O_WRONLY)) < 0) { // FIFO를 쓰기 전용으로 열기
+    unlink(FIFOFILE); // 기존 FIFO 파일 삭제
+
+    if (mkfifo(FIFOFILE, 0666) < 0) { // 새로운 FIFO 파일 생성
+        perror("mkfifo()");
+        return -1;
+    }
+
+    if ((fd = open(FIFOFILE, O_RDONLY)) < 0) { // FIFO를 읽기 전용으로 열기
         perror("open()");
         return -1;
     }
 
+    FILE *fp;
+    char command[BUFSIZ];
+
     while (1) {
-        int sensor_id = rand() % 2 + 1; // 1에서 3 사이의 랜덤 sensor_id 생성
-        int reading;
-        if (sensor_id == 1) {
-            reading = rand() % 61 - 20; // sensor_id 1의 reading은 -20에서 40 사이 값
-        } else if (sensor_id == 2) {
-            reading = rand() % 101; // sensor_id 2의 reading은 0에서 100 사이 값
-        } else {
-            reading = rand() % 21; // sensor_id 3의 reading은 0에서 20 사이 값
+        int n = read(fd, buf, sizeof(buf)); // FIFO로부터 데이터를 읽음
+
+        if (n > 0) {
+            buf[n] = '\0'; // Null-terminate 문자열
+
+            // 파이썬 스크립트를 호출하여 FIFO로부터 받은 데이터를 전달하고 MariaDB에 데이터 삽입
+            sprintf(command, "python3 %s \"%s\"", PYTHON_SCRIPT, buf);
+            fp = popen(command, "r");
+            if (fp == NULL) {
+                printf("Failed to execute Python script.\n");
+                return -1;
+            }
+            char result_buffer[1024]; // 출력 결과를 담을 버퍼
+
+            while (fgets(result_buffer, sizeof(result_buffer), fp) != NULL) {
+                printf("%s", result_buffer); // 파이썬 스크립트의 출력 결과를 터미널에 출력
+            }
+
+            pclose(fp);
         }
-
-        // FIFO로 sensor_id와 reading 데이터 전송
-        dprintf(fd, "%d %d\n", sensor_id, reading);
-
-        sleep(1); // 1초 대기
     }
 
     close(fd);
