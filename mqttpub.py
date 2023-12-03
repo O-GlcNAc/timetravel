06a99cc9b845705f6bdd38636ddfff68cc8a7919
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-12-03 17:00:14 +0900

    Update mqttpub.py

--- mqttpub.py
+++ mqttpub.py
@@ -15,28 +15,38 @@ conn = pymysql.connect(
     database='exam'
 )
 
-cursor = conn.cursor()
+while True:
+    cursor = conn.cursor()
 
-# SensorData 테이블에서 가장 최신의 데이터 가져오기
-query = """
-    SELECT sensor_id, temperature, humidity, illuminance, timestamp
-    FROM SensorData
-    ORDER BY timestamp DESC
-    LIMIT 1
-"""
+    # SensorData 테이블에서 가장 최신의 데이터 가져오기
+    query = """
+        SELECT sensor_id, temperature, humidity, illuminance, timestamp
+        FROM SensorData
+        ORDER BY timestamp DESC
+        LIMIT 1
+    """
 
-cursor.execute(query)
-latest_data = cursor.fetchone()
+    cursor.execute(query)
+    latest_data = cursor.fetchone()
 
-# 만약 latest_data가 존재하면 MQTT 브로커로 전송
-if latest_data:
-    client = mqtt.Client()
-    client.connect(broker_address, broker_port)
+    # 만약 latest_data가 존재하면 MQTT 브로커로 전송
+    if latest_data:
+        client = mqtt.Client()
+        client.connect(broker_address, broker_port)
+        
+        sensor_id, temperature, humidity, illuminance, timestamp = latest_data
+        message = f"sensor_id: {sensor_id}, temperature: {temperature}, humidity: {humidity}, illuminance: {illuminance}, timestamp: {timestamp}"
+        
+        client.publish(topic, message)
+        print(f"Published: {message}")
+
+    conn.commit()
+    conn.close()
     
-    sensor_id, temperature, humidity, illuminance, timestamp = latest_data
-    message = f"sensor_id: {sensor_id}, temperature: {temperature}, humidity: {humidity}, illuminance: {illuminance}, timestamp: {timestamp}"
-    
-    client.publish(topic, message)
-    print(f"Published: {message}")
-
-conn.close()
+    time.sleep(5)  # 5초 대기 후 다음 루프 실행
+    conn = pymysql.connect(
+        host='localhost',
+        user='scott',
+        password='tiger',
+        database='exam'
+    )
