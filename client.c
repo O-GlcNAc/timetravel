e16e923fe9200e2e2f7dd362204911f05a59a3c9
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-12-03 20:46:24 +0900

    Update client.c

--- client.c
+++ client.c
@@ -8,11 +8,8 @@
 
 int main(int argc, char **argv) {
     int fd;
-
-    struct tm *tm_current = localtime(&current_time);
-    strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", tm_current);
-
-
+    time_t current_time = time(NULL); // 현재 시간으로 초기화
+    char timestamp[20];
 
     if ((fd = open(FIFOFILE, O_WRONLY)) < 0) { // FIFO를 쓰기 전용으로 열기
         perror("open()");
@@ -25,13 +22,12 @@ int main(int argc, char **argv) {
         int humidity;
         int temperature;
         int luminance;
-        char timestamp[20];
 
         // 현재 시간을 "YYYY-MM-DD HH:MM:SS" 형식으로 변환
         struct tm *tm_current = localtime(&current_time);
         strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", tm_current);
 
-
+        // 각 센서 ID에 따라 데이터 생성
         if (sensor_id == 1) {
             reading = rand() % 61 - 20; // sensor_id 1의 reading은 -20에서 40 사이 값
             humidity = 10;
@@ -52,10 +48,10 @@ int main(int argc, char **argv) {
         // FIFO로 sensor_id, reading, temperature, humidity, luminance, timestamp 데이터 전송
         dprintf(fd, "%d %d %d %d %d %s\n", sensor_id, reading, temperature, humidity, luminance, timestamp);
 
-         // 현재 시간을 10분 증가시킴
+        // 현재 시간을 10분 증가시킴
         tm_current->tm_min += 10;
         current_time = mktime(tm_current); // 업데이트된 tm 구조체를 다시 time_t로 변환
-        
+
         sleep(1); // 1초 대기
     }
 
