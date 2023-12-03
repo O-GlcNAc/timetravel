8c172dd3814f13e1b8fa1fffbcaadba6056a8920
O-GlcNAc <53387948+O-GlcNAc@users.noreply.github.com>
2023-12-03 20:21:37 +0900

    Update client.c

--- client.c
+++ client.c
@@ -8,7 +8,8 @@
 
 int main(int argc, char **argv) {
     int fd;
-    srand(time(NULL)); // 난수 생성을 위한 시드 설정
+    struct tm start_time = { .tm_year = 123, .tm_mon = 11, .tm_mday = 3, .tm_hour = 0, .tm_min = 0, .tm_sec = 0 }; // 2023-12-03 00:00:00
+    time_t current_time = mktime(&start_time);
 
     if ((fd = open(FIFOFILE, O_WRONLY)) < 0) { // FIFO를 쓰기 전용으로 열기
         perror("open()");
@@ -21,6 +22,11 @@ int main(int argc, char **argv) {
         int humidity;
         int temperature;
         int luminance;
+        char timestamp[20];
+
+        // 현재 시간을 "YYYY-MM-DD HH:MM:SS" 형식으로 변환
+        strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", localtime(&current_time));
+
         if (sensor_id == 1) {
             reading = rand() % 61 - 20; // sensor_id 1의 reading은 -20에서 40 사이 값
             humidity = 10;
@@ -38,9 +44,10 @@ int main(int argc, char **argv) {
             luminance = 30;
         }
 
-        // FIFO로 sensor_id와 reading 데이터 전송
-        dprintf(fd, "%d %d %d %d %d\n", sensor_id, reading,temperature,humidity,luminance);
+        // FIFO로 sensor_id, reading, temperature, humidity, luminance, timestamp 데이터 전송
+        dprintf(fd, "%d %d %d %d %d %s\n", sensor_id, reading, temperature, humidity, luminance, timestamp);
 
+        current_time += 600; // 10분 더하기
         sleep(1); // 1초 대기
     }
 
